aa02287b647fef8101e9360f1314a269e2fe0922
Savelii Zagurskii <saveliyzagurskiy@gmail.com>
2016-09-29 16:23:12 +0300

    Added normal onCreate()

--- app/src/main/java/com/example/strelok/testapp1/ScrumActivity.java
+++ app/src/main/java/com/example/strelok/testapp1/ScrumActivity.java
@@ -4,14 +4,35 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class ScrumActivity extends AppCompatActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+
+    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        init();
+    }
+
+    /**
+     * Same as onCreate(Bundle) but called for those activities created with the attribute android.R.attr.persistableMode set to persistAcrossReboots.
+     *
+     * @param savedInstanceState if the activity is being re-initialized after previously being shut down then this Bundle contains the data it
+     *                           most recently supplied in onSaveInstanceState.
+     *                           Note: Otherwise it is null.
+     * @param persistentState    if the activity is being re-initialized after previously being shut down or powered off then this
+     *                           Bundle contains the data it most recently supplied to outPersistentState in onSaveInstanceState.
+     *                           Note: Otherwise it is null.
+     */
+    @Override public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
 
+        init();
+    }
+
+    private void init() {
         setContentView(R.layout.dashboard);
 
         String log;
